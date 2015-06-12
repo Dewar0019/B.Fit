e8f08ce9f7c4382d9c5767edcdbaae00d109bcb4
Dewar Tan <Dewar0019@gmail.com>
2015-06-12 14:36:32 -0400

    fixed button for inserting data fields

--- client/arms/arms.js
+++ client/arms/arms.js
@@ -7,15 +7,13 @@ if(Meteor.isClient){
 	})
 
 	Template.arms.events({
-
-		'click .addExercise': function(event){
+		'submit #addExercise': function(event){
 			event.preventDefault();
 			console.log("Exercise Added");
 			var exercise = event.target.exercise.value;
 			var sets = event.target.numOfSets.value;
 			var reps = event.target.numOfReps.value;
 			var weight = event.target.weight.value;
-
 			Meteor.call('addArmExerciseToDB', exercise, sets, reps, weight);
 		}
 	});
