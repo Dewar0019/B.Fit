7ab1d9a95d02ca6ec6e69ae0511538e4aa155f66
sbernheim4 <sambernheim@gmail.com>
2015-06-16 13:56:17 -0400

    added running page

---
+++ client/running/running.js
@@ -0,0 +1,57 @@
+if(Meteor.isClient){
+
+	Template.running.helpers({
+
+		'runningExercise': function(){
+			return RunningCardio.find({}, {sort: {runningSets: -1}});
+		},
+    	'selectedClass': function (){
+      		var playerId = this._id;
+      		var selectedExer = Session.get('selectedExer');
+      		if(playerId == selectedExer){
+        	return "selected"
+      		}
+     	},
+      	'showSelectedExer': function(){
+      		var selectedExer = Session.get('selectedExer');
+      		return RunningCardio.findOne(selectedExer)
+    	}
+
+	});
+
+	Template.running.events({
+    
+		'submit #addRunningExercise': function(event){
+
+			event.preventDefault();
+			console.log("Exercise Added");
+
+			var runningTime = event.target.runningTime.value;
+			var runningSpeed = event.target.runningSpeed.value;
+			var runningDistance = event.target.runningDistance.value;
+			Meteor.call('addRunningCardioToDB', runningTime, runningSpeed, runningDistance);
+		},
+
+    	'click .setsp': function(){
+      	var playerId = this._id;
+      	Session.set('selectedExer', playerId);
+      	var selectedSet = Session.get('selectedExer');
+      	RunningCardio.update(selectedSet, {$inc: {runningTime: 1}}); 
+    	},
+
+    	'click .repsp': function(){
+      	var playerId = this._id;
+      	Session.set('selectedExer', playerId);
+      	var selectedReps = Session.get('selectedExer');
+      	RunningCardio.update(selectedReps, {$inc: {runningSpeed: 1}});
+    	},
+
+    	'click .setsd': function(){
+    	var playerId = this._id;
+      	Session.set('selectedExer', playerId);
+      	var selectedSet = Session.get('selectedExer');
+        RunningCardio.update(selectedSet, {$inc: {runningDistance: -1}});
+    	}
+	});
+
+}
