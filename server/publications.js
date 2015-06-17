63130f2851c1eca0c38cd89e82e09d3aff48d7e9
Limin Wang <ponny616@brandeis.edu>
2015-06-17 13:04:33 -0400

    fix

--- server/publications.js
+++ server/publications.js
@@ -6,13 +6,10 @@ Meteor.publish('coreExercises', function(){return CoreExercises.find();});
 Meteor.publish('ellipticalWorkout', function(){return EllipticalWorkout.find();});
 Meteor.publish('theProfiles', function(){return Profiles.find();});
 Meteor.publish('runningCardio', function(){return RunningCardio.find();});
-<<<<<<< HEAD
 Meteor.publish('theRoutines', function() {return Routines();});
-=======
 Meteor.publish('stairMasterWorkout', function(){return StairMasterWorkout.find();});
 Meteor.publish('cyclingCardio', function(){return CyclingCardio.find();});
 
->>>>>>> d4f827f80722ece85a7e871aa1423dfc357b3a6f
 Meteor.publish('userExercises', function () { return Meteor.users.find({ _id: this.userId }, { fields: { savedExercises: 1 } });});
 
 Meteor.users.allow({
