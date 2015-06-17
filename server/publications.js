c50807b57aadeed0fee49d740030670d37bc89ad
sbernheim4 <sambernheim@gmail.com>
2015-06-16 21:42:30 -0400

    fixed the server/methods file

--- server/publications.js
+++ server/publications.js
@@ -25,67 +25,6 @@ if (Meteor.isServer){
 	console.log("Hello Server");
 
 	Meteor.methods({
-		'addArmExerciseToDB': function(armExercise, armSets, armReps, armWeight){
-			ArmExercises.insert({
-				Name: armExercise, 
-				Sets: armSets,
-				Reps: armReps,
-				Weight: armWeight
-			});
-		},
-
-		'addChestExerciseToDB': function(chestExercise, chestSets, chestReps, chestWeight){
-			ChestExercises.insert({
-				Name: chestExercise, 
-				Sets: chestSets,
-				Reps: chestReps,
-				Weight: chestWeight
-			});
-		},
-
-
-		'addLegExerciseToDB': function(legExercise, legSets, legReps, legWeight){
-			LegExercises.insert({
-				Name: legExercise, 
-				Sets: legSets,
-				Reps: legReps,
-				Weight: legWeight
-			});
-		},
-
-		'addCoreExerciseToDB': function(coreExercise, coreSets, coreReps, coreWeight){
-			CoreExercises.insert({
-				Name: coreExercise, 
-				Sets: coreSets,
-				Reps: coreReps,
-				Weight: coreWeight
-			});
-		},
-
-		'addRunningCardioToDB': function(runningTime, runningSpeed, runningDistance){
-			RunningCardio.insert({
-				runningTime: runningTime,
-				runningSpeed: runningSpeed,
-				runningDistance: runningDistance
-			});
-		},
-
-		'increment': function (armSets, armReps){
-
-		},
-
-		'removeExercise': function(exercise) {
-			var arr = Meteor.user().savedExercises;
-			console.log(arr);
-			for(var i = 0; i<arr.length; i++) {
-				console.log(arr[i]._id);
-				if(arr[i]._id == exercise) {
-					console.log("done");
-					arr.splice(i, 1);
-					return;
-				}
-			}
-		},
 
         'addRoutine': function(nameOfRoutine) {
             
@@ -99,15 +38,6 @@ if (Meteor.isServer){
                 console.log(savedExer.push(selectedExer));
                 Meteor.users.update( { _id: Meteor.userId() }, { $set: { "savedExercises": savedExer}});
             }
-		},
-
-		'addEllipticalWorkoutToDB': function(ellipticalTime , ellipticalSpeed , ellipticalDistance){
-			EllipticalWorkout.insert({
-				time: new Date(), 
-				ellipticalTime: ellipticalTime, 
-				ellipticalSpeed: ellipticalSpeed,
-				ellipticalDistance: ellipticalDistance
-			});
 		}
 	})
 }
