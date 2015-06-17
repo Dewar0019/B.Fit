d5a5e528cdae96bbefdcc7634d9480eeded145fa
Limin Wang <ponny616@brandeis.edu>
2015-06-17 10:06:16 -0400

    changes

--- client/running/running.js
+++ client/running/running.js
@@ -32,13 +32,45 @@ if(Meteor.isClient){
 			Meteor.call('addRunningCardioToDB', parseInt(runningTime), parseInt(runningSpeed), parseInt(runningDistance));
 		},
 
-    	'click .setsp': function(){
-      	var playerId = this._id;
-      	Session.set('selectedExer', playerId);
-      	var selectedSet = Session.get('selectedExer');
-      	RunningCardio.update(selectedSet, {$inc: {runningTime: 1}}); 
-    	}
+    	'click .timep': function(){
+      var playerId = this._id;
+      Session.set('selectedExer', playerId);
+      var selectedTime = Session.get('selectedExer');
+      RunningCardio.update(selectedTime, {$inc: {workoutTime: 1}}); 
+     },
+
+    'click .timed': function(){
+      var playerId = this._id;
+      Session.set('selectedExer', playerId);
+      var selectedTime = Session.get('selectedExer');
+      RunningCardio.update(selectedTime, {$inc: {workoutTime: -1}});
+      },
+      'click .speedp': function(){
+      var playerId = this._id;
+      Session.set('selectedExer', playerId);
+      var selectedSpeed = Session.get('selectedExer');
+      RunningCardio.update(selectedSpeed, {$inc: {Speed: 1}});
+      },
       
+    'click .speedd': function(){
+      var playerId = this._id;
+      Session.set('selectedExer', playerId);
+      var selectedSpeed = Session.get('selectedExer');
+      RunningCardio.update(selectedSpeed, {$inc: {Speed: -1}});
+      },
+      'click .distancep': function(){
+      var playerId = this._id;
+      Session.set('selectedExer', playerId);
+      var selectedDistance = Session.get('selectedExer');
+      RunningCardio.update(selectedDistance, {$inc: {Distance: 1}});
+      },
+      
+    'click .distanced': function(){
+      var playerId = this._id;
+      Session.set('selectedExer', playerId);
+      var selectedDistance = Session.get('selectedExer');
+      RunningCardio.update(selectedDistance, {$inc: {Distance: -1}});
+      }
 	});
 
 }
