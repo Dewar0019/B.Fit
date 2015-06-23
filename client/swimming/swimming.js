1ad0bab1100a583c80a385eea1cdccdd3a3672b0
Amy <ashein2@brandeis.edu>
2015-06-23 10:54:40 -0400

    Swimming

--- client/swimming/swimming.js
+++ client/swimming/swimming.js
@@ -1,78 +1,66 @@
 if(Meteor.isClient){
 
-	Template.swimming.helpers({
+  Template.swimming.helpers({
 
-		'swimmingWorkout': function(){
-			return SwimmingWorkout.find({}, {sort: {swimmingTime: -1}});
-		},
-    	'selectedClass': function (){
-      		var playerId = this._id;
-      		var selectedExer = Session.get('selectedExer');
-      		if(playerId == selectedExer){
-        	return "selected"
-      		}
-     	},
-      	'showSelectedExer': function(){
-      		var selectedExer = Session.get('selectedExer');
-      		return swimmingWorkout.findOne(selectedExer)
-    	}
+    'swimmingWorkout': function(){
+      return SwimmingWorkout.find({}, {sort: {swimmingTime: -1}});
+    },
+      'selectedClass': function (){
+          var playerId = this._id;
+          var selectedExer = Session.get('selectedExer');
+          if(playerId == selectedExer){
+          return "selected"
+          }
+      },
+        'showSelectedExer': function(){
+          var selectedExer = Session.get('selectedExer');
+          return swimmingWorkout.findOne(selectedExer)
+      }
 
-	});
+  });
 
-	Template.swimming.events({
+  Template.swimming.events({
 
-		'submit #addSwimmingWorkout': function(event){
+    'submit #addSwimmingWorkout': function(event){
 
-			event.preventDefault();
-			console.log("Exercise Added");
+      event.preventDefault();
+      console.log("Exercise Added");
 
-			var swimmingTime = event.target.swimmingTime.value;
-			var swimmingLaps = event.target.swimmingLaps.value;
-			var swimmingDistance = event.target.swimmingDistance.value;
-			Meteor.call('addSwimmingWorkoutToDB', parseInt(swimmingTime) , parseInt(swimmingLaps) , parseInt(swimmingDistance));
-		 
+      var swimmingTime = event.target.swimmingTime.value;
+      var swimmingLaps = event.target.swimmingLaps.value;
+      var swimmingDistance = event.target.swimmingLaps.value * 25;
+      Meteor.call('addSwimmingWorkoutToDB', parseInt(swimmingTime) , parseInt(swimmingLaps) , parseInt(swimmingDistance));
+     
       },
 
     'click .timep': function(){
       var playerId = this._id;
-     	Session.set('selectedExer', playerId);
+      Session.set('selectedExer', playerId);
       var selectedTime = Session.get('selectedExer');
       SwimmingWorkout.update(selectedTime, {$inc: {workoutTime: 1}}); 
-   	 },
+     },
 
     'click .timed': function(){
       var playerId = this._id;
       Session.set('selectedExer', playerId);
       var selectedTime = Session.get('selectedExer');
       SwimmingWorkout.update(selectedTime, {$inc: {workoutTime: -1}});
-     	},
+      },
 
     'click .distancep': function(){
       var playerId = this._id;
       Session.set('selectedExer', playerId);
       var selectedSpeed = Session.get('selectedExer');
       SwimmingWorkout.update(selectedSpeed, {$inc: {Laps: 1}});
-      },
-    	
-    'click .distanced': function(){
-     	var playerId = this._id;
-      Session.set('selectedExer', playerId);
-      var selectedDistance = Session.get('selectedExer');
-      SwimmingWorkout.update(selectedDistance, {$inc: {Laps: -1}});
-     	},
-
-    'click .distancep': function(){
-      var playerId = this._id;
-      Session.set('selectedExer', playerId);
-      var selectedDistance = Session.get('selectedExer');
-      SwimmingWorkout.update(selectedDistance, {$inc: {Distance: 25}});
+      SwimmingWorkout.update(selectedSpeed, {$inc: {Distance: 25}});
       },
       
     'click .distanced': function(){
       var playerId = this._id;
       Session.set('selectedExer', playerId);
-      var selectedDistance = Session.get('selectedExer');
-      SwimmingWorkout.update(selectedDistance, {$inc: {Distance: -25}});
+      var selectedSpeed = Session.get('selectedExer');
+      SwimmingWorkout.update(selectedSpeed, {$inc: {Laps: -1}});
+      SwimmingWorkout.update(selectedSpeed, {$inc: {Distance: -25}});
       },
 
     'click .addTo' :function() {
@@ -82,6 +70,6 @@ if(Meteor.isClient){
       Meteor.call('addToRoutine', tt);
       console.log("called");
       }
-	});
+  });
 
 }
