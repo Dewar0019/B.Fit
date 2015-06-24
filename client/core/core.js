690b47d6cf068efdb751d7855674d57bfa277014
Amy <ashein2@brandeis.edu>
2015-06-24 11:16:09 -0400

    delete button for rest of strength

--- client/core/core.js
+++ client/core/core.js
@@ -68,7 +68,13 @@ if(Meteor.isClient){
       	Session.set('selectedExer', playerId);
       	var selectedReps = Session.get('selectedExer');
       	CoreExercises.update(selectedReps, {$inc: {Reps: -1}});
-    	}
+    	},
+      'click .remove': function(){
+        var playerId = this._id;
+        Session.set('selectedInfo',playerId);
+        var selectedInfo = Session.get('selectedInfo');
+        CoreExercises.remove(selectedInfo);
+      }
 	});
 
 }
