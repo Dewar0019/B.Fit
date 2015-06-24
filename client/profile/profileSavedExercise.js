108b4d326f19b98bac330e7bdbdb49ce0ac6c654
Limin Wang <ponny@Limins-MacBook-Pro.local>
2015-06-24 14:49:34 -0400

    img

--- client/profile/profileSavedExercise.js
+++ client/profile/profileSavedExercise.js
@@ -6,8 +6,9 @@ Template.profileSavedExercises.helpers({
 			return getSelected.exercises;
 	},
 
-	
-
+	photo:function(){ // returns the URL of the gravatar photo for this email
+		return Gravatar.imageUrl(Gravatar.hash(this.emails[0].address,{secure:true}))
+	}
 })
 
 Template.profileSavedExercises.events({
