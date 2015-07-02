0fc684c59ff7c498dc168d9fbfa497024e5b8e40
sbernheim4 <sambernheim@gmail.com>
2015-07-02 11:52:15 -0400

    moved depreceated pages to an oldPages folder and began work on welcome page after login

---
+++ client/welcome.js
@@ -0,0 +1,5 @@
+Template.welcome.helpers({
+	photo:function(){ // returns the URL of the gravatar photo for this email
+		return Gravatar.imageUrl(Gravatar.hash(Meteor.user().emails[0].address,{secure:true}))
+	}
+})
