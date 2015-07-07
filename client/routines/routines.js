50520df1fdfa34886819280811de6754d9025be3
Dewar Tan <Dewar0019@gmail.com>
2015-07-06 23:14:39 -0400

    routines now route to specify pages for exercises to be displayed

--- client/routines/routines.js
+++ client/routines/routines.js
@@ -1,3 +1,3 @@
 Template.routines.helpers({
-	item: function() {return Routines.find({}).fetch()}
+	item: function() {return Routines.find({}).fetch()}  //Gotta change later so that it consist only of presets and user inserted routines
 })
