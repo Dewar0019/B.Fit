23fde577d0baabd3ca405ed426eaad6490fdf328
Amy Sheinhait <ashein@Amys-MacBook-Pro.local>
2015-07-07 20:10:56 -0400

    took out search

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -1,18 +1,25 @@
-Session.setDefault("searchCategory", "All");
-
-Template.layout.events({
-    'submit #searchForm': function (event) {
-        event.preventDefault();
-        var s = searchBox.value;
-        Session.set("searchTerm", s);
-
-
-        Router.go('/search/' + Session.get("searchTerm"));
-     },
-     'click #searchBox': function(event) {
-         searchBox.value = '';
-     }
-});
-
+ // Session.setDefault("All");
+ 
+ // Template.layout.events({
+ // 	'submit #searchForm': function(event) {
+ // 		event.preventDefault();
+ // 		var s = searchBox.value;
+ // 		Session.set("searchTerm",s);
+ 
+ 		
+ // 		Router.go(Session.get("searchTerm"));
+ // 	},
+ // 	// changes category in the dropdown
+ // 	'click #selectSearchType li a':function(event) {
+ // 		event.preventDefault();
+ // 		$(event.target).parents('.btn-group').find('.btn').text($(event.target).text());
+ //   		$(event.target).parents('.btn-group').find('.btn').val($(event.target).text());
+ 
+ //   		// append the caret
+ //   		$(event.target).parents('.btn-group').find('.btn').append('&nbsp;<span class="caret"></span>');
+ 
+ //   		Session.set("searchCategory",$(event.target).text());
+ // 	}
+ // }); 
 
 
--- client/layout/search.js
+++ client/layout/search.js
@@ -1,10 +1,15 @@
-Exercises.initEasySearch(['exerciseName','category'],
-	{
-		'limit':30
-	});
-
-Template.search.helpers({
-	searchTerm:function(){
-		return Session.get("searchTerm");
-	}
-});
+// Template.search.helpers({
+//  	searchTerm:function(){
+//  		return Session.get("searchTerm");
+//  	}
+//  });
+ 
+//  Template.search.events({
+//  	'click .nav-tabs a':function(event) {
+//  		event.preventDefault();
+//  		$(event.target).tab('show');
+//  		//console.log($(event.target).text());
+//  		Session.set("searchCategory",$(event.target).text());
+//  	}
+//  });
+//  
