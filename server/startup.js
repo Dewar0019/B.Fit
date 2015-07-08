7846ae7a3bf11bf25603322b23b269aa9fb636ad
sbernheim4 <sambernheim@gmail.com>
2015-07-08 11:03:31 -0400

    Modified initial Exercises insert function

--- server/startup.js
+++ server/startup.js
@@ -2,78 +2,52 @@ if(Meteor.isServer){
 
 	Meteor.startup(function(){
 		if (Exercises.find().fetch()==0){
+			
 			Exercises.insert({
 				Category:"arms",
 				Name: "Bicep Curl",
-				Sets: 4,
-				Reps: 15, 
-				Weight: 20
 			});
 
 			Exercises.insert({
 				Category:"arms",
 				Name: "Hammer Curl",
-				Sets: 2,
-				Reps: 12,
-				Weight: 15
 			});
 
 
 			Exercises.insert({
 				Category:"arms",
 				Name: "Reverse Curl",
-				Sets: 3,
-				Reps: 10,
-				Weight: 25
 			});
 
 
 			Exercises.insert({
 				Category:"chest",
 				Name: "Chest Press",
-				Sets: 4,
-				Reps: 15, 
-				Weight: 20
 			});
 
 			Exercises.insert({
 				Category:"chest",
 				Name: "Dips",
-				Sets: 2,
-				Reps: 12,
-				Weight: 15
 			});
 
 			Exercises.insert({
 				Category:"chest",
 				Name: "Cable Fly",
-				Sets: 3,
-				Reps: 10,
-				Weight: 25
 			});
 
 			Exercises.insert({
 				Category:"legs",
 				Name: "Squats",
-				Sets: 4,
-				Reps: 15, 
-				Weight: 160
 			});
 
 			Exercises.insert({
 				Category:"legs",
 				Name: "Step Ups",
-				Sets: 2,
-				Reps: 12,
-				Weight: 15
 			});
 
 			Exercises.insert({
 				Category:"legs",
 				Name: "Calf Raises",
-				Sets: 3,
-				Reps: 10,
-				Weight: 25
 			});
 		}
 
@@ -182,7 +156,6 @@ if(Meteor.isServer){
 						Weight: 20
 					},
 				],
-
 				createdAt: new Date()
 			})
 		}
