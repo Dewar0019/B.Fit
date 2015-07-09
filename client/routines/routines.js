09b61ed667ef9d50afa94344b12bb6a06d2bf9e1
Dewar Tan <Dewar0019@gmail.com>
2015-07-08 20:13:42 -0400

    added completed exercise button

--- client/routines/routines.js
+++ client/routines/routines.js
@@ -1,3 +1,11 @@
 Template.routines.helpers({
 	item: function() {return Routines.find({}).fetch()}  //Gotta change later so that it consist only of presets and user inserted routines
 })
+
+Template.routines.events({
+	'click #setForCompleted' :function() {
+		var routine = this;
+		console.log(routine);
+		Session.set('forCompletedRoutine', routine);
+	}
+})
