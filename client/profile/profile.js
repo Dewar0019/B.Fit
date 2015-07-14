8a922b263a8ddfb59b18a4f51241656385d8694d
Limin Wang <ponny@Limins-MacBook-Pro.local>
2015-07-14 15:08:57 -0400

    profile

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -1,9 +1,9 @@
-
 Template.profile.helpers({
 	photo:function(){ // returns the URL of the gravatar photo for this email
 		return Gravatar.imageUrl(Gravatar.hash(Meteor.user().emails[0].address,{secure:true}))
 	},
-	getUserInfo: function() { return Meteor.user();}
+	getUserInfo: function() { return Meteor.user();},
+	getEmail: function() {return Meteor.user().emails[0].address}
 })
 
 Template.profile.events({
@@ -11,3 +11,4 @@ Template.profile.events({
     AccountsTemplates.logout();
   }
 });
+
