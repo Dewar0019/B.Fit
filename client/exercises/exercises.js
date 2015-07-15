b6d68ff3bdbd948c772f31d3cd3222660f398ac0
Amy Sheinhait <ashein@Amys-MacBook-Pro.local>
2015-07-15 11:57:28 -0400

    maybe useful

--- client/exercises/exercises.js
+++ client/exercises/exercises.js
@@ -1,11 +1,16 @@
 Template.exercises.helpers({
 	getExercises: function () { return Exercises.find().fetch();},
 	startExercise: function() { return Session.get('selectedRoutine').exercises.length > 0},
+	getCardio: function () { return Cardio.find().fetch();},
 
-
-	'addorEditForm': function(){
-		return Session.get("addOrEdit");
+	'strengthOrCardioForm': function(){
+		return Session.get("strengthOrCardio");
 	}
+
+	isCardio : function() { 
+		var thisExercise = Session.get("storeExercise");
+		return thisExercise.type.indexOf("Cardio") > -1;
+	},
 });
 
 
@@ -24,18 +29,27 @@ Template.exercises.events({
 
 	},
 
-	'click #viewAdd': function (event){
-		Session.set("addOrEdit", true);
+	'click #viewAddStrength': function (event){
+		Session.set("strengthOrCardio", true);
 
-		document.getElementById("viewAdd").className = "tab-item active";
-		document.getElementById("viewEdit").className = "tab-item";
+		document.getElementById("viewAddStrength").className = "tab-item active";
+		// document.getElementById("viewEdit").className = "tab-item";
+		document.getElementById("viewAddCardio").className = "tab-item";
 	},
 
-	'click #viewEdit': function (event){
-		Session.set("addOrEdit", false);
+	// 'click #viewEdit': function (event){
+	// 	Session.set("strengthOrCardio", false);
 
-		document.getElementById("viewAdd").className = "tab-item";
-		document.getElementById("viewEdit").className = "tab-item active";
+	// 	document.getElementById("viewAddStrength").className = "tab-item";
+	// 	document.getElementById("viewEdit").className = "tab-item active";
+	// 	document.getElementById("viewCardio").className = "tab-item";
+	// },
+	'click #viewAddCardio': function (event){
+		Session.set("strengthOrCardio", false);
+
+		document.getElementById("viewAddStrength").className = "tab-item";
+		// document.getElementById("viewEdit").className = "tab-item";
+		document.getElementById("viewAddCardio").className = "tab-item active";
 	},
 
 })
