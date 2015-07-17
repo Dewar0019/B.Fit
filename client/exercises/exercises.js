f80c82bb1188f3013458237ee27a18f797adad88
Amy Sheinhait <ashein@Amys-MacBook-Pro.local>
2015-07-17 11:19:27 -0400

    comments added

--- client/exercises/exercises.js
+++ client/exercises/exercises.js
@@ -1,40 +1,25 @@
 
-
 Template.exercises.helpers({
-	getExercises: function () { return Exercises.find().fetch();},
-	getCardio: function () { return Cardio.find().fetch();},
-	
-	showExerciseList2 : function() {
-		return Session.get("showExerciseList2");
-	},
-	
-	exerciseList2: function() {
-		
+	getExercises: function () { return Exercises.find().fetch();}, //shows strength exercises
+	getCardio: function () { return Cardio.find().fetch();},  //shows cardio exercises
+	// 	return Session.get("showExerciseList2");
+	// },
+	exerciseList2: function() {  //shows exercise list for edit
 		currentRoutines = Session.get('selectedRoutine');
-
 		console.log("exerciseList2")
 		console.log(Session.get('selectedRoutine'));
-
 		return currentRoutines.exercises;
 	},
 	
-	'strengthOrCardioForm': function(){
+	'strengthOrCardioForm': function(){ //choice between strength and cardio
 		return Session.get("strengthOrCardio");
 	},
-
-	isCardio : function() { 
-		var thisExercise = Session.get("storeExercise");
-		return thisExercise.type.indexOf("Cardio") > -1;
-	},
-
-	'editOrNot': function(){
+	'editOrNot': function(){ //choice between edit and other
 		return Session.get("editOrNot");
 	},
 });
 
-
 Template.exercises.events({
-
 	'click .selectedExercise' : function() {
 		var exercise = this;
 		console.log(exercise);
@@ -47,7 +32,6 @@ Template.exercises.events({
 		Session.set('storeExercise', (cardio.Type.indexOf("Cardio") > 1));
 
 	},
-
 	'click #viewAddStrength': function (event){
 		Session.set("strengthOrCardio", true);
 		Session.set("editOrNot", false);
@@ -56,10 +40,7 @@ Template.exercises.events({
 		document.getElementById("viewAddStrength").className = "tab-item active";
 		document.getElementById("viewAddCardio").className = "tab-item";
 		document.getElementById("viewEdit").className = "tab-item";
-
-		console.log(":P")
 	},
-
 	'click #viewEdit': function (event){
 		Session.set("editOrNot", true);
 
@@ -68,10 +49,6 @@ Template.exercises.events({
 		document.getElementById("viewAddStrength").className = "tab-item";
 		document.getElementById("viewEdit").className = "tab-item active";
 		document.getElementById("viewAddCardio").className = "tab-item";
-
-		console.log(Session.get("selectedRoutine"));
-		console.log(":D")
-
 	},
 	'click #viewAddCardio': function (event){
 		Session.set("strengthOrCardio", false);
@@ -81,8 +58,6 @@ Template.exercises.events({
 		document.getElementById("viewAddStrength").className = "tab-item";
 		document.getElementById("viewAddCardio").className = "tab-item active";
 		document.getElementById("viewEdit").className = "tab-item";
-
-		console.log(":)")
 	},
 
 })
