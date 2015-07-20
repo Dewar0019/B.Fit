00a736a299b5000b5ec2826c413c749d1c1a1e43
Dewar Tan <Dewar0019@gmail.com>
2015-07-20 13:41:09 -0400

    changed field for analytics date and removed old images

---
+++ mobile-config.js
@@ -0,0 +1,6 @@
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#000000');
+
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
