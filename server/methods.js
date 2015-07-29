0255bef1bc6c70037dae3e42306d77e142881b60
Dewar Tan <Dewar0019@gmail.com>
2015-07-29 14:08:33 -0400

    made comments and removed console log statements

--- server/methods.js
+++ server/methods.js
@@ -1,31 +1,28 @@
 Meteor.methods({
+
+	//This will compile the average time and number of times the user attempted the Routine
 	"compileFinished" :function (routine) {
 		var counter = 0;
 		var averageTime = 0;
 		var allCompleted = Completed.find({_uID: Meteor.userId(), routineName: routine.routineName}).fetch();
+		//Checks all the completed exercises in collection
 		allCompleted.forEach(function(obj) {
-			averageTime += obj.timeToComplete;
+			averageTime += obj.timeToComplete; 
 			counter++;
 		});
-		var avgTotalSeconds = averageTime/allCompleted.length;
-		console.log("avgTotalSeconds");
-		console.log(avgTotalSeconds);
-		var completeTimeAverage = (Math.floor(avgTotalSeconds / 3600) + ":"
+		var avgTotalSeconds = averageTime/allCompleted.length; //Averages the Time taken to complete exercise
+		var completeTimeAverage = (Math.floor(avgTotalSeconds / 3600) + ":" //Presents average time in format xx:xx:xx
 		+ Math.floor(avgTotalSeconds / 60 % 60 / 10) + Math.floor(avgTotalSeconds / 60 % 60 % 10) + ":"
 		+ parseInt(avgTotalSeconds % 60 / 10) + parseInt(avgTotalSeconds % 60 % 10));
-		console.log(completeTimeAverage);
+		//Updates the routine with the updated values for times completed and average time to complete/attempt workout
 		Routines.update({_id: routine._id}, {$set:{ timesCompleted: counter, avgComplete: completeTimeAverage}});
-		console.log(routine);
 	},
 
 	'updateWeight' : function(weight) {
-		if(this.userId) {
+		if(this.userId) { //Ensures this is the correct userID
 		var newProfile = Meteor.users.findOne(Meteor.userId());
-		console.log(newProfile.profile.currentWeight);
 		newProfile.profile.currentWeight = weight;
-		console.log(newProfile.profile.currentWeight);
 		Meteor.users.update(Meteor.userId(), {$set: {profile: newProfile.profile}});
-		console.log(Meteor.user().profile.currentWeight);
 		}
 	},
 
@@ -49,18 +46,11 @@ Meteor.methods({
 		});
 	},
 
-	// var justCreated = Routines.findOne({_uID: Meteor.userId(), routineName: name});
-	// console.log(justCreated);
-	// return justCreated;
-	// Session.set('recentAdd', Routines.findOne({_uID: Meteor.userId(), routineName: name}));
-
 	// FOR STRENGTH EXERCISES
 	'addToRoutine': function(exercise, getRoutine, sets, reps, weight) {  //this will add new exercise to routine
 		var currentTime = new Date(); //Grab the current time
 		console.log("in the method.js file 1");
 		console.dir(exercise);
-
-
 
 		Intermediate.insert({ //This is the actual object that will be added to the routine
 			Name: exercise.Name,
@@ -119,9 +109,6 @@ Meteor.methods({
 
 
 function determineType(exercise) {
-	// var allRoutines = Session.get("grabAllRoutines");
-	// for (var routine in allRoutines){
-	// 	for (var exercise in routine.exercises){
 	console.log("the exercise object will be printed below ")
 	console.dir(exercise);
 
